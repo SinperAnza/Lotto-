4838eedd5d739d874962fe67c61a57036aee3430
Anzas <Anzas@ACE>
2024-08-24 15:19:41 +0200

    Add project files.

---
+++ Lotto@/Form1.Designer.cs
@@ -0,0 +1,200 @@
+﻿namespace Lotto_
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.txtNumber1 = new System.Windows.Forms.TextBox();
+            this.txtNumber2 = new System.Windows.Forms.TextBox();
+            this.txtNumber3 = new System.Windows.Forms.TextBox();
+            this.txtNumber4 = new System.Windows.Forms.TextBox();
+            this.txtNumber5 = new System.Windows.Forms.TextBox();
+            this.txtNumber6 = new System.Windows.Forms.TextBox();
+            this.btnGuess = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.toolTip1 = new System.Windows.Forms.ToolTip(this.components);
+            this.lblResult = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.lblCost = new System.Windows.Forms.Label();
+            this.lblGuesses = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // txtNumber1
+            // 
+            this.txtNumber1.Location = new System.Drawing.Point(57, 103);
+            this.txtNumber1.MaxLength = 2;
+            this.txtNumber1.Name = "txtNumber1";
+            this.txtNumber1.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber1.TabIndex = 0;
+            this.toolTip1.SetToolTip(this.txtNumber1, "Enter a number a number between 1 and 45");
+            // 
+            // txtNumber2
+            // 
+            this.txtNumber2.Location = new System.Drawing.Point(211, 104);
+            this.txtNumber2.MaxLength = 2;
+            this.txtNumber2.Name = "txtNumber2";
+            this.txtNumber2.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber2.TabIndex = 1;
+            this.toolTip1.SetToolTip(this.txtNumber2, "Enter anumber between 1 and 45");
+            this.txtNumber2.TextChanged += new System.EventHandler(this.textBox2_TextChanged);
+            // 
+            // txtNumber3
+            // 
+            this.txtNumber3.Location = new System.Drawing.Point(359, 104);
+            this.txtNumber3.MaxLength = 2;
+            this.txtNumber3.Name = "txtNumber3";
+            this.txtNumber3.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber3.TabIndex = 2;
+            this.toolTip1.SetToolTip(this.txtNumber3, "Enter a number between 1 and 45");
+            // 
+            // txtNumber4
+            // 
+            this.txtNumber4.Location = new System.Drawing.Point(525, 104);
+            this.txtNumber4.MaxLength = 2;
+            this.txtNumber4.Name = "txtNumber4";
+            this.txtNumber4.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber4.TabIndex = 3;
+            this.toolTip1.SetToolTip(this.txtNumber4, "Enter a number between 1 and 45 ");
+            // 
+            // txtNumber5
+            // 
+            this.txtNumber5.Location = new System.Drawing.Point(717, 103);
+            this.txtNumber5.MaxLength = 2;
+            this.txtNumber5.Name = "txtNumber5";
+            this.txtNumber5.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber5.TabIndex = 4;
+            this.toolTip1.SetToolTip(this.txtNumber5, "Enter a number between 1 and 45 ");
+            // 
+            // txtNumber6
+            // 
+            this.txtNumber6.Location = new System.Drawing.Point(886, 103);
+            this.txtNumber6.MaxLength = 2;
+            this.txtNumber6.Name = "txtNumber6";
+            this.txtNumber6.Size = new System.Drawing.Size(100, 22);
+            this.txtNumber6.TabIndex = 5;
+            this.toolTip1.SetToolTip(this.txtNumber6, "Enter a number between 1 and 45 ");
+            // 
+            // btnGuess
+            // 
+            this.btnGuess.Location = new System.Drawing.Point(104, 336);
+            this.btnGuess.Name = "btnGuess";
+            this.btnGuess.Size = new System.Drawing.Size(177, 39);
+            this.btnGuess.TabIndex = 6;
+            this.btnGuess.Text = "Guess ";
+            this.btnGuess.UseVisualStyleBackColor = true;
+            this.btnGuess.Click += new System.EventHandler(this.btnGuess_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(512, 336);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 39);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Reset ";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // lblResult
+            // 
+            this.lblResult.AutoSize = true;
+            this.lblResult.Location = new System.Drawing.Point(569, 234);
+            this.lblResult.Name = "lblResult";
+            this.lblResult.Size = new System.Drawing.Size(7, 16);
+            this.lblResult.TabIndex = 8;
+            this.lblResult.Text = "\r\n";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(859, 336);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(165, 39);
+            this.button1.TabIndex = 9;
+            this.button1.Text = "next";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // lblCost
+            // 
+            this.lblCost.AutoSize = true;
+            this.lblCost.Location = new System.Drawing.Point(572, 254);
+            this.lblCost.Name = "lblCost";
+            this.lblCost.Size = new System.Drawing.Size(0, 16);
+            this.lblCost.TabIndex = 10;
+            // 
+            // lblGuesses
+            // 
+            this.lblGuesses.AutoSize = true;
+            this.lblGuesses.Location = new System.Drawing.Point(313, 253);
+            this.lblGuesses.Name = "lblGuesses";
+            this.lblGuesses.Size = new System.Drawing.Size(0, 16);
+            this.lblGuesses.TabIndex = 11;
+            this.lblGuesses.Click += new System.EventHandler(this.lblGuesses_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1101, 450);
+            this.Controls.Add(this.lblGuesses);
+            this.Controls.Add(this.lblCost);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblResult);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.btnGuess);
+            this.Controls.Add(this.txtNumber6);
+            this.Controls.Add(this.txtNumber5);
+            this.Controls.Add(this.txtNumber4);
+            this.Controls.Add(this.txtNumber3);
+            this.Controls.Add(this.txtNumber2);
+            this.Controls.Add(this.txtNumber1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtNumber1;
+        private System.Windows.Forms.TextBox txtNumber2;
+        private System.Windows.Forms.TextBox txtNumber3;
+        private System.Windows.Forms.TextBox txtNumber4;
+        private System.Windows.Forms.TextBox txtNumber5;
+        private System.Windows.Forms.TextBox txtNumber6;
+        private System.Windows.Forms.Button btnGuess;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.ToolTip toolTip1;
+        private System.Windows.Forms.Label lblResult;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label lblCost;
+        private System.Windows.Forms.Label lblGuesses;
+    }
+}
+
---
+++ Lotto@/Form1.cs
@@ -0,0 +1,153 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lotto_
+{
+    public partial class Form1 : Form
+    {
+        private List<int> LottoNumbers;
+        private const decimal CostPerGuess = 20.00m;
+
+
+        public Form1()
+        {
+            InitializeComponent();
+            GenerateLottoNumbers();
+        }
+        private void GenerateLottoNumbers()
+        {
+            Random rand = new Random();
+            LottoNumbers = new List<int>();
+            while (LottoNumbers.Count < 6)
+            {
+                int number = rand.Next(1, 46);
+                if (!LottoNumbers.Contains(number))
+                {
+                    LottoNumbers.Add(number);
+                }
+            }
+
+        }
+
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnGuess_Click(object sender, EventArgs e)
+        {
+            List<int> userGuess = new List<int>();
+            int num1, num2, num3, num4, num5, num6;
+
+            if (int.TryParse(txtNumber1.Text, out num1) &&   
+                int.TryParse(txtNumber2.Text, out num2) &&
+                int.TryParse(txtNumber3.Text, out num3) &&
+                int.TryParse(txtNumber4.Text, out num4) &&
+                int.TryParse(txtNumber5.Text, out num5) &&
+                int.TryParse(txtNumber6.Text, out num6))
+            {
+
+
+                userGuess.AddRange(new[] { num1, num2, num3, num4, num5, num6 });
+
+                if (userGuess.Any(num => num < 1 || num > 45))
+                {
+                    MessageBox.Show("Each number must be between 1 and 45.");
+                    return;
+
+                }
+
+
+                int correctCount = LottoNumbers.Intersect(userGuess).Count();
+                lblResult.Text = "You matched " + correctCount + " numbers.";
+
+                decimal totalCost = CalculateTotalCost(userGuess.Count);
+                lblCost.Text = "Total Cost: R" + totalCost.ToString("F2");
+                lblGuesses.Text = "Number of Guesses: " + userGuess.Count;
+
+
+                if (correctCount == 6)
+                {
+                    MessageBox.Show("Congratulations! You have won!");
+                    this.BackColor = Color.Green;
+                }
+
+            }
+            else
+            {
+                MessageBox.Show("Please enter valid numbers.");
+            }
+
+
+        }
+
+        private static decimal CalculateTotalCost(int numberOfGuesses)
+        {
+            return numberOfGuesses * CostPerGuess;
+        }
+
+
+
+
+
+
+        private void btnReset_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            GenerateLottoNumbers();
+
+            lblResult.Text = "";
+            txtNumber1.Clear();
+            txtNumber2.Clear();
+            txtNumber3.Clear();
+            txtNumber4.Clear();
+            txtNumber5.Clear();
+            txtNumber6.Clear();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Form2 form = new Form2();
+            form.Show();
+            this.Hide();
+        }
+
+        private void lblGuesses_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void UpdateFormColor(int correctGuesses)
+        {
+            if (correctGuesses >= 4)
+            {
+                this.BackColor = Color.Yellow;
+            }
+            else
+            {
+                this.BackColor = Color.Red;
+            }
+        }
+    }
+}
+        
+    
+
+
+      
